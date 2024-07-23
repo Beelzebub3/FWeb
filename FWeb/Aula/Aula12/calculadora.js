6c7fb96a7345a10d04670c978a36cb97a5b5d62c
Beelzebub3 <davinascimentojobs@gmail.com>
2024-07-22 21:54:57 -0300

    Add files via upload

--- FWeb/Aula/Aula12/calculadora.js
+++ FWeb/Aula/Aula12/calculadora.js
@@ -4,24 +4,38 @@ function addNumber(Number) {
     textValue += Number;
     updateText();
 };
+function erase() {
+    textValue = textValue.substring(0, textValue.length - 1);
+    updateText();
+};
 function addOperator(Operator) {
     
+    textValue += Operator;
     updateText();
 };
 function addDecimal() {
-
+    
+    textValue += '.';
     updateText();
 };
 function clearText() {
-
+    textValue = '';
     updateText();
 };
 function updateText() {
 
-    document.querySelector('*display').value = textValue;
+    document.querySelector('#display').value = textValue;
 }; 
 
 function calculate() {
 
+    textValue = eval(textValue);
+    if (isNaN){
+        // When the user clicks on <div>, open the popup
+        var popup = document.getElementById("myPopup");
+        popup.classList.toggle("show");
+
+        document.getElementById('tudo').style.backgroundImage = "./Aula12/gojo_purple.jpg"
+  }
+    }
     updateText();
-};

c66c5ef7a284c2fa3a341ec9f3099da8a6e53285
Beelzebub3 <davinascimentojobs@gmail.com>
2024-07-24 21:59:48 -0300

    Add files via upload

---
+++ Aula/Aula14/Exemplo1/index.js
@@ -0,0 +1,15 @@
+let testeArray = ["Kiss-Shot_Acerola-Onion_Heart-Underblade","Senjougahara_Hitagi","Jhonny_Joestar"]
+
+for(const contador in testeArray){
+    testeArray[contador];
+}
+for(let i=0; i<=10; i++)
+{
+    testeArray[i];
+}
+let cont =0;
+while (cont!=10)
+{
+    testeArray[cont];
+    cont++;
+}
---
+++ Aula/Aula14/Exercicio2/exer2.js
@@ -0,0 +1,23 @@
+fetch("./exer2.json")
+.then(response=>response.json())
+.then(pessoas=>{
+    for(const pessoa in pessoas)
+    {
+        let newName = document.createElement("p");
+        newName.innerHTML = pessoas[pessoa].nome;
+        document.querySelector('p').appendChild(newName);
+
+        let newAge= document.createElement("p");
+        newAge.innerHTML = pessoas[pessoa].idade;
+        document.querySelector('p').appendChild(newAge);
+
+        let newCpf= document.createElement("p");
+        newCpf.innerHTML = pessoas[pessoa].cpf;
+        document.querySelector('p').appendChild(newCpf);
+
+        let newTel= document.createElement("p");
+        newTel.innerHTML = pessoas[pessoa].telefone;
+        document.querySelector('p').appendChild(newTel);
+    }
+}
+);

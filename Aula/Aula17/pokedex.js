c48f2a393c6b89a3643995a1cd114cdb53bf1a79
Beelzebub3 <davinascimentojobs@gmail.com>
2024-08-02 21:48:05 -0300

    Add files via upload

--- Aula/Aula17/pokedex.js
+++ Aula/Aula17/pokedex.js
@@ -1,14 +1,17 @@
 const imgpoke = document.querySelector('#img_pokemon');
 const nomepoke = document.querySelector('#nome_poke');
 const numpoke = document.querySelector('#num_poke');
-const poketipo = document.querySelector('#tipo');
+const poketipo1 = document.querySelector('#tipo1');
+const poketipo2 = document.querySelector('#tipo2');
 const pokepeso = document.querySelector('#peso');
-const pokecry = document.querySelector('#poke_cry');
+const pokecry = document.querySelector('#pokechoro');
 const pokealtura = document.querySelector('#altura');
 const ButtonNext = document.querySelector('#Next');
 const ButtonPreview = document.querySelector('#Preview');
 const input = document.querySelector('#input_name');
-const form = document.querySelector('#form_busca');
+const form = document.querySelector('.form_busca');
+
+let idPokemon = 1;
 
 const fetchPokemon = async(pokemon) =>{
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -17,19 +20,43 @@ const fetchPokemon = async(pokemon) =>{
 }
 const showPoke = async(pokemon) => {
     const dataPoke = await fetchPokemon(pokemon);
+    idPokemon = dataPoke.id;
     imgpoke.src = dataPoke.sprites.front_default;
     nomepoke.innerHTML = dataPoke.name;
-    pokecry.innerHTML = dataPoke.cries.latest;
+    pokecry.src = dataPoke.cries.latest;
     numpoke.innerHTML = dataPoke.id;
     pokealtura.innerHTML = dataPoke.height*10;
     pokepeso.innerHTML = dataPoke.weight/10;
-    poketipo.innerHTML = dataPoke.types[0].type.name;
+    poketipo1.innerHTML = dataPoke.types[0].type.name;
+    if(dataPoke.types.length>1){
+        poketipo2.innerHTML = dataPoke.types[1].type.name;
+    }
+    else
+    {
+        poketipo2.innerHTML = '';
+    }
 }
 
 ButtonPreview.addEventListener("click", () => {
-    showPoke(eval(idPokemon-'1'));
+    if(idPokemon>1)
+    {
+        idPokemon -=1;
+        showPoke(idPokemon);
+    }
 });
 ButtonNext.addEventListener("click", () => {
-    showPoke(eval(idPokemon+'1'));
+    idPokemon+=1;
+    showPoke(idPokemon);
 });
-showPoke('40');
+
+input.addEventListener("input", () => {
+    idPokemon = toString(input.value);
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    showPoke(input.value.toLowerCase());
+}
+)
+
+showPoke(idPokemon);
